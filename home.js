36737a5da322a650f8b717bef7e641844465212d
LucielVitorBS <luciel.vitor.bs@gmail.com>
2018-05-19 13:31:15 -0300

    Primeiro Commit que dei no projeto

---
+++ home.js
@@ -0,0 +1,27 @@
+// console.log("oi xuxu")
+
+// tipoDesenvolvedor = "expert"
+
+// switch (tipoDesenvolvedor) {
+//     case "expert":
+//         console.log("Nossa, sou um expert")
+//         break
+// }
+
+// numero1 = 3
+// numero2 = 2
+
+// total = numero1 + numero2
+
+// function soma(primeiroNumero, segundoNumero) {
+//     total = primeiroNumero + segundoNumero
+//     console.log(total)
+// }
+
+// soma(numero1, numero2)
+
+let soma = function (parametro1, parametro2) {
+    console.log(parametro1 + parametro2)
+}
+
+soma(1, 5)
